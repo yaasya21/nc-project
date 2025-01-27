c99beb8478d324b19fa43e4324a32548f7c5ee26
root <root@LAPTOP-PJGA4M89.>
2025-01-27 13:04:33 +0000

    get information about endpoints

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,11 +1,13 @@
 const endpointsJson = require("../endpoints.json");
+const server = require("../server");
+const request = require('supertest');
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
+    return request(server)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const server = express();
+const { getApi } = require("./controllers/api.controller");
 
 server.use(express.json());
+
+server.get("/api", getApi); 
+
+server.use((err, req, res, next) => {
+    if ((err.code === "ENOENT")) {
+        console.log(err);
+        res.status(500).send({ msg: "Internal Server Error" });
+      } else {
+        next(err);
+      }
+    });
 
 module.exports = server;
