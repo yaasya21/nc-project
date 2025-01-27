6aa4200c957e18f44d2ee70371a1dbbad3ee088c
root <root@LAPTOP-PJGA4M89.>
2025-01-27 15:51:35 +0000

    get an article with a certain id

---
+++ controllers/articles.controller.js
@@ -0,0 +1,12 @@
+const { selectArticleById } = require("../models/article.model");
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article: article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
