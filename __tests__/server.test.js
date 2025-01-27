469e8bbc66d9b722c394c5d1d166451d74f3d88b
root <root@LAPTOP-PJGA4M89.>
2025-01-27 15:14:55 +0000

    get all topics

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,9 +1,20 @@
 const endpointsJson = require("../endpoints.json");
 const server = require("../server");
-const request = require('supertest');
-/* Set up your test imports here */
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const db = require("../db/connection");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeAll(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET /api-topics", () => {
+  test("404: Responds with an error NotFound when Unavailable Route is passed", () => {
+    return request(server)
+      .get("/api-topics")
+      .expect(404)
+  });
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -12,6 +23,30 @@ describe("GET /api", () => {
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api", () => {
+  test("200: Responds with  array of topic objects, each of which should have the following properties: slug, description", () => {
+    return request(server)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics).toEqual([
+          {
+            description: "The man, the Mitch, the legend",
+            slug: "mitch",
+          },
+          {
+            description: "Not dogs",
+            slug: "cats",
+          },
+          {
+            description: "what books are made of",
+            slug: "paper",
+          },
+        ]);
       });
   });
 });
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const server = express();
 const { getApi } = require("./controllers/api.controller");
+const { getTopics } = require("./controllers/topics.controller");
 
 server.use(express.json());
 
 server.get("/api", getApi); 
 
-server.use((err, req, res, next) => {
-    if ((err.code === "ENOENT")) {
-        console.log(err);
-        res.status(500).send({ msg: "Internal Server Error" });
-      } else {
-        next(err);
-      }
-    });
+server.get("/api/topics", getTopics); 
 
 module.exports = server;
