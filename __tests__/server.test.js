f05ab4dde84c7cb3a5f6a63e1eb551968f94efaf
root <root@LAPTOP-PJGA4M89.>
2025-01-28 18:14:02 +0000

    get all users

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -111,6 +111,25 @@ describe("GET /api/articles", () => {
   });
 });
 
+describe("GET /api/users", () => {
+  test("200: Responds with array of user objects", () => {
+    return request(server)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body: { users } }) => {
+        expect(users.length).toBe(4);
+
+        users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
 describe("GET /api/articles/:article_id/comments", () => {
   test("200: Responds with an array of comment objects for the given article_id", () => {
     return request(server)
