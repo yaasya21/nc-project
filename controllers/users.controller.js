ec196f08ccaf28f65eddf2ba15d171698ec3b842
root <root@LAPTOP-PJGA4M89.>
2025-01-30 12:10:17 +0000

    get a user by username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,18 @@
-const { selectUsers } = require("../models/users.model");
+const { selectUsers, selectUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
-    selectUsers().then((users) => {
+  selectUsers().then((users) => {
     res.status(200).send({ users: users });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUsername(username)
+    .then((user) => {
+      res.status(200).send({ user: user });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
--- models/users.model.js
+++ models/users.model.js
@@ -1,7 +1,24 @@
 const db = require("../db/connection");
+const { checkIfExists } = require("../utils/checkIfExists");
 
 exports.selectUsers = () => {
   return db.query("SELECT * FROM users").then(({ rows }) => {
     return rows;
   });
+};
+
+exports.selectUsername = (username) => {
+  if (username.length > 40) {
+    return Promise.reject({
+      status: 400,
+      msg: `Bad request`,
+    });
+  }
+  return checkIfExists(username, "users", "username")
+    .then(() => {
+      return db.query("SELECT * FROM users WHERE username = $1", [username]);
+    })
+    .then(({ rows }) => {
+      return rows[0];
+    });
 };
